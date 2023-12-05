dace7410d4f3e8a8a0afe1bd94bdb901fe8a0e56
alessandrofuda <alessandro.fuda@gmail.com>
2023-12-05 18:22:07 +0100

    first comm

---
+++ alghotitm.php
@@ -0,0 +1,22 @@
+<?php
+
+$input = file_get_contents('input.txt');
+$array = array_filter(explode("\n", $input));
+
+$tot = 0;
+
+foreach ($array as $item) {
+    preg_match_all('/[0-9]/', $item, $matches);
+    $integers = $matches[0];
+
+    if(!empty($integers)) {
+        $first_int = $integers[0];
+        $last_int = $integers[count($integers)-1];
+        $calibration_val = (integer) $first_int.$last_int;
+        $tot += (integer) $calibration_val;
+    }
+}
+
+echo $tot."\n";
+
+
