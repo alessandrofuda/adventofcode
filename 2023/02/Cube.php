8753ee7fe35f1ca2bf42d65c06d727f90b124868
alessandrofuda <alessandro.fuda@gmail.com>
2023-12-06 18:50:10 +0100

    day2 ..wip..

--- 2023/02/Cube.php
+++ 2023/02/Cube.php
@@ -11,6 +11,33 @@ class Cube
 
     public function run() : int
     {
-        //
+        $games = $this->convertInputToArr();
+        $validated_games = [];
+
+        foreach ($games as $game) {
+            $sets = $this->getSets($game);
+
+            $break = false;
+
+            foreach ($sets as $set) {
+                if(!$this->passingChecks($set)){ // check colors/thresholds
+                    $break = true;
+                    break;
+                }
+            }
+
+            if(!$break) {
+                $validated_games[] = $this->getGameId($game);
+            }
+        }
+
+        $tot = $this->sumAllItemsOfArray($validated_games);
+        return $tot;
+
+    }
+
+    private function getGameId(mixed $game) : int
+    {
+        // return $id;
     }
 }
